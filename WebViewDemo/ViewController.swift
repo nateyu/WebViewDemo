8b9409b006c4447806fee5cad9f8f67bac0533e4
Chen Yihu <chenyihu@boohee.com>
2016-06-20 10:20:32 +0800

    Initial Commit

---
+++ WebViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewDemo
+//
+//  Created by Chen Yihu on 6/20/16.
+//  Copyright © 2016 CYH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
